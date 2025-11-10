5a9b45b45c1a590bfb1f2e90420255d13d62d195
lss_win11 <lushisong@126.com>
2025-11-10 08:17:01 +0800

    Fix large files issue

--- BO/worker.cpp
+++
@@ -1,8 +0,0 @@
-#include "worker.h"
-#include "systementity.h"
-
-Worker::Worker(SystemEntity& entity) : systemEntity(entity) {}
-
-void Worker::workerSolve(const QString& modelDescription, const QList<TestItem>& testItemList, int truncateMode) {
-    systemEntity.completeSolve(modelDescription, testItemList, truncateMode);
-}
--- tools/test_function_data.sql
+++
@@ -1,9 +0,0 @@
--- 为测试数据迁移脚本创建示例Function数据
-
-INSERT INTO Function (FunctionID, FunctionName, ExecsList, CmdValList, Remark) VALUES
-(1, '电源供电功能', '电源模块, 保险丝, 电源开关', 'Power.state=ON', '检测电源供电是否正常'),
-(2, '电机驱动功能', '电机驱动器, 电机, 传感器', 'Motor.speed=100', '检测电机驱动功能'),
-(3, '信号采集功能', '传感器1, AD转换器, 信号处理模块', 'Signal.enabled=TRUE', '检测信号采集功能');
-
--- 验证插入
-SELECT * FROM Function;
